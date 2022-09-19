d91e8482f4153160756da502f878db7e82b8f7f3
kouta1206 <ko.bump1206@icloud.com>
2022-09-19 14:20:42 +0900

    API作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/events', to: 'events#index'
+  # get '/events/:id', to: 'events#show'
+  # post '/events', to: 'events#create'
+  # put '/events/:id', to: 'events#update'
+  # delete '/events/:id', to: 'events#destroy'
+  resource :events, only: [:index, :show, :create, :update, :destroy]
 end
